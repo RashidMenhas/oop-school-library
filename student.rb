a2a193111ce114676db34952b92f75460de3a0b4
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-18 17:21:42 +0500

    Student class is child class which inherited from person class

---
+++ student.rb
@@ -0,0 +1,11 @@
+require_relative 'person'
+class Student < Person
+  def initialize(age, name, parents_permission, classroom)
+    super(age, name, parents_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯(ツ)/¯'
+  end
+end
