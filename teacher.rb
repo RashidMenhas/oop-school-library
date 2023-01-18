c80d40f2affc09635ba753d00c53bc05998eea3b
RashidMenhas <rashidmenhas10@gmail.com>
2023-01-18 17:22:52 +0500

    Teacher class also is a children class it is inheret from person class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require_relative 'person'
+
+class Teacher < person
+  def initialize(specialization, age, name, parents_permission)
+    super(age, name, parents_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services
+    true
+  end
+end
